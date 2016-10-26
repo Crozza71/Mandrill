533522a4a3152a8e3a92326eaefbc541e47b5149
Paul Crosby <paul71crosby@gmail.com>
2016-10-26 17:31:06 +0100

    sendmail

--- app.js
+++ app.js
@@ -3,8 +3,110 @@ var app        = express();
 var mandrill = require('mandrill-api/mandrill');
 var mandrill_client = new mandrill.Mandrill('wOw8h_wydGj7qFRkjKJLNQ');
 
-var template_name = "Organiser-Login"
-var template_content =[
+
+
+function sendMail(template_name, template_content, to_variable){
+    console.log(template_name)
+    console.log(template_content)
+
+    var message = {
+        "subject": "New Login114 ",
+        "from_email": "paul@stampede.co.uk",
+        "from_name": "Paul Crosby",
+        "to": to_variable,
+        "headers": {
+            "Reply-To": "message.reply@example.com"
+        },
+        "important": false,
+        "track_opens": null,
+        "track_clicks": null,
+        "auto_text": null,
+        "auto_html": null,
+        "inline_css": null,
+        "url_strip_qs": null,
+        "preserve_recipients": null,
+        "view_content_link": null,
+        "bcc_address": null,
+        "tracking_domain": null,
+        "signing_domain": null,
+        "return_path_domain": null,
+        "merge": true,
+        "merge_language": "mailchimp",
+        "global_merge_vars": [{
+                "name": "merge1",
+                "content": "<h1>MERGING</h1>"
+            }],
+        "merge_vars": [{
+                "rcpt": "paul@stampede.co.uk",
+                "vars": [{
+                        "name": "lname",
+                        "content": ""
+                    }]
+            }],
+        "tags": [
+            "password-resets"
+        ],
+        // "subaccount": "customer-123",
+        // "google_analytics_domains": [
+        //     "example.com"
+        // ],
+        // "google_analytics_campaign": "message.from_email@example.com",
+        // "metadata": {
+        //     "website": "www.example.com"
+        // },
+        // "recipient_metadata": [{
+        //         "rcpt": "recipient.email@example.com",
+        //         "values": {
+        //             "user_id": 123456
+        //         }
+        //     }],
+        // "attachments": [{
+        //         "type": "text/plain",
+        //         "name": "myfile.txt",
+        //         "content": "ZXhhbXBsZSBmaWxl"
+        //     }],
+        // "images": [{
+        //         "type": "image/png",
+        //         "name": "IMAGECID",
+        //         "content": "ZXhhbXBsZSBmaWxl"
+        //     }]
+    };
+    var async = false;
+    var ip_pool = "Main Pool";
+    // var send_at = "example send_at";
+    mandrill_client.messages.sendTemplate({"template_name":template_name,"template_content":template_content,"message": message, "async": async, "ip_pool": ip_pool }, function(result) {
+        console.log(result);
+        /*
+        [{
+                "email": "recipient.email@example.com",
+                "status": "sent",
+                "reject_reason": "hard-bounce",
+                "_id": "abc123abc123abc123abc123abc123"
+            }]
+        */
+    }, function(e) {
+        // Mandrill returns the error as an object with name and message keys
+        console.log('A mandrill error occurred: ' + e.name + ' - ' + e.message);
+        // A mandrill error occurred: Unknown_Subaccount - No subaccount exists with the id 'customer-123'
+    });
+
+}
+
+sendMail('Index', [ {"name":"Test", "content":"im here"}], [{
+                "email": "kin_bing@hotmail.com",
+                "name": "Harry Howard",
+                "type": "to"
+            }])
+
+
+
+
+
+var template_name = "Index"
+
+
+/*var template_content =[
+    
     {
         "name": "header",
         "content": "<h3>Hello *|FNAME|*</h3><h6>*|EMAIL|*</h6>"
@@ -12,7 +114,7 @@ var template_content =[
     },
     {
         "name": "results",
-        "content": "<p> *|FNAME|* Congratulations you are a winner!<p>"    
+        "content": "<p> *|FNAME|* Congratulations you completed the event!<p>"    
     },
     {
         "name": "welcome",
@@ -24,11 +126,13 @@ var template_content =[
     },
     {
        "name": "greeting",
-       "content": "Regards<br>Stampede Team"  
+       "content": "the lads"  
     },
     {"name": "footer",
         "content":"copyright@2016 SBAR Endurance Running team"
     },
+    {"name":"imhereandnotused",
+        content:"notshown"}
 ];
 
 var message = {
@@ -96,25 +200,11 @@ var message = {
     //         "name": "IMAGECID",
     //         "content": "ZXhhbXBsZSBmaWxl"
     //     }]
-};
+};*/
 var async = false;
 var ip_pool = "Main Pool";
 // var send_at = "example send_at";
-mandrill_client.messages.sendTemplate({"template_name":template_name,"template_content":template_content,"message": message, "async": async, "ip_pool": ip_pool }, function(result) {
-    console.log(result);
-    /*
-    [{
-            "email": "recipient.email@example.com",
-            "status": "sent",
-            "reject_reason": "hard-bounce",
-            "_id": "abc123abc123abc123abc123abc123"
-        }]
-    */
-}, function(e) {
-    // Mandrill returns the error as an object with name and message keys
-    console.log('A mandrill error occurred: ' + e.name + ' - ' + e.message);
-    // A mandrill error occurred: Unknown_Subaccount - No subaccount exists with the id 'customer-123'
-});
+
 
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
